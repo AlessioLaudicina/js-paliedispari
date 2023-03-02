7bfb7e19720347e7062fb9efa88a7ff47c7f8c09
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-03-02 17:23:47 +0100

    function pariDispari

--- js/palidrome.js
+++ js/palidrome.js
@@ -4,19 +4,15 @@
 */
 
 let parolaUtente = prompt('inserisci una parola');
-let parolaDivisa = parolaUtente.split('')
-console.log(parolaDivisa)
+let parolaDivisa = parolaUtente.split('');
+console.log(parolaDivisa);
 
  parolaDivisa = parolaDivisa.reverse();
- console.log(parolaDivisa)
+ console.log(parolaDivisa);
 
  let parolaInvertita = parolaDivisa.join('');
- console.log(parolaInvertita)
+ console.log(parolaInvertita);
 
- if(parola == parolaInvertita){
-    console.log('la parola è palindroma')
- } else {
-    console.log('la parola non è palindroma')
- }
+
 
  
