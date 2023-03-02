88e9071b55c16d4e42762763cc6a4e887207a7c6
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-03-02 16:26:35 +0100

    palindrome

--- js/palidrome.js
+++ js/palidrome.js
@@ -3,6 +3,20 @@
 - Creare una funzione per capire se la parola inserita è palindroma 
 */
 
+let parolaUtente = prompt('inserisci una parola');
+let parolaDivisa = parolaUtente.split('')
+console.log(parolaDivisa)
 
+ parolaDivisa = parolaDivisa.reverse();
+ console.log(parolaDivisa)
+
+ let parolaInvertita = parolaDivisa.join('');
+ console.log(parolaInvertita)
+
+ if(parola == parolaInvertita){
+    console.log('la parola è palindroma')
+ } else {
+    console.log('la parola non è palindroma')
+ }
 
  
